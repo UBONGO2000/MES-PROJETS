d67702f7174bf9bf50f9f45ca0ba0428264967e2
UBONGO2000 <gadyubongo@gmail.com>
2024-12-03 06:39:57 +0100

    first commit

---
+++ symfony_init/src/Controller/FirstController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first',name:'first')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig',[
+            'Controller_name' => 'FirstController',
+        ]);
+
+        // return new Response(
+        //     "<h1>Hello new response</h1>"
+        // );
+    }
+}
