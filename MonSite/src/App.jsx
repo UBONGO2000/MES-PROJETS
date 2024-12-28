d3d668702bdcb4289cafb7ae49f872553db08bde
UBONGO2000 <gadyubongo@gmail.com>
2024-12-28 21:49:40 +0100

    fin heroSection et debut de FeaturesSection

--- MonSite/src/App.jsx
+++ MonSite/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from "./coponents/Navbar.jsx";
 import HeroSection  from "./coponents/HeroSection.jsx";
+import FeatureSection from "./coponents/FeatureSection.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,10 @@ function App() {
         <Navbar/>
         <div className="max-w-7xl mx-auto pt-20 px-6">
             <HeroSection/>
+            <FeatureSection/>
+
         </div>
+
     </>
   )
 }
