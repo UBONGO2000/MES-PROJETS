eba7e7f0bb8fb7155628a9c36e6c911ee1b5bf45
UBONGO2000 <gadyubongo@gmail.com>
2024-12-29 01:22:21 +0100

    fin heroSection,debut et fin de FeaturesSection, Pricing, testimonialset footer: fin du projet

--- MonSite/src/App.jsx
+++ MonSite/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
 import Navbar from "./coponents/Navbar.jsx";
 import HeroSection  from "./coponents/HeroSection.jsx";
 import FeatureSection from "./coponents/FeatureSection.jsx";
+import Workflow from "./coponents/Workflow.jsx";
+import Pricing from "./coponents/Pricing.jsx";
+import Testimonials from "./coponents/Testimonials.jsx";
+import Footer from "./coponents/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -12,6 +14,10 @@ function App() {
         <div className="max-w-7xl mx-auto pt-20 px-6">
             <HeroSection/>
             <FeatureSection/>
+            <Workflow/>
+            <Pricing/>
+            <Testimonials/>
+            <Footer/>
 
         </div>
 
